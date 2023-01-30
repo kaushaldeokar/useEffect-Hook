1fd82f6f57207a2f818e0989248705e6f6a3e170
kaushaldeokar <kaushal.deokar@gmail.com>
2023-01-30 13:29:54 +0530

    without mapping of data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
-
-import './App.css';
-import NavBar from './component/NavBar';
-
+import "./App.css";
+import NavBar from "./component/NavBar";
+import React, { useState, useEffect } from "react";
 function App() {
+  const [State, setState] = useState(2 );
+  const [data1, setData] = useState([]);
+  function incre() {
+    setState(State + 1);
+  }
+  function decre() {
+    setState(State - 1);
+  }
+  useEffect(() => {
+    // window.alert("useEffect called");
+     async function getData(){
+      const get = await fetch('https://reqres.in/api/users?page=${State}');
+     const res=get.json();
+     setData(res);
+     console.log(data1);
+    }
+    getData();
+
+  }, [State,data1]);
+  //array passed shows no dependencies
   return (
     <div className="App">
-      <NavBar/>
+      <NavBar />
+      <div className="container" style={{display:"flex-column",justifyContent:"center",padding:"10%"}}>
+        <h1>{State}</h1>
+        <button onClick={incre}> Increment </button>
+        <button onClick={decre}> Decrement </button>
+        {
+          // data1.map((e,ind)=>{
+          //   return(
+          //     <div>
+          //       <h4>{e.first_name}</h4>
+          //       <h4>{}</h4>
+          //     </div>
+          //   )
+          // })
+        }
+      </div>
     </div>
   );
 }
