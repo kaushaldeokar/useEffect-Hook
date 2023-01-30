79eac040f64e87f91a1f3e1fae75dc79dc290c39
kaushaldeokar <kaushal.deokar@gmail.com>
2023-01-30 20:17:05 +0530

    final verdict error solved response.data

--- src/App.js
+++ src/App.js
@@ -1,44 +1,61 @@
 import "./App.css";
 import NavBar from "./component/NavBar";
 import React, { useState, useEffect } from "react";
+
 function App() {
-  const [State, setState] = useState(2 );
+  const [state, setState] = useState(1);
   const [data, setData] = useState([]);
   function incre() {
-    setState(State + 1);
+    if (state + 1 <= 2) setState(state + 1);
   }
+
   function decre() {
-    setState(State - 1);
+    if (state > 1) setState(state - 1);
   }
+
+
+
+  async function getData() {
+    const get = await fetch("https://reqres.in/api/users?page=" + state);
+    const res = await get.json();
+    console.log(res.data);
+    setData(res.data);
+    //  console.log(res);
+  }
+
+
+  
+
   useEffect(() => {
     // window.alert("useEffect called");
-     async function getData(){
-      const get = await fetch('https://reqres.in/api/users?page=${State}');
-     const res=await get.json();
-     setData(res);
-     console.log(res);
-    }
     getData();
+  }, [state]);
 
-  }, [State]);
+
   //array passed shows no dependencies
   return (
     <div className="App">
       <NavBar />
-      <div className="container" style={{display:"flex-column",justifyContent:"center",padding:"10%"}}>
-        <h1>{State}</h1>
-        <button onClick={incre}> Increment </button>
-        <button onClick={decre}> Decrement </button>
-        {
-          // data.map((e,ind)=>{
-          //   return(
-          //     <div>
-          //       <h4>{e.first_name}</h4>
-          //       <h4>{}</h4>
-          //     </div>
-          //   )
-          // })
-        }
+      <div
+        className="container"
+        style={{
+          display: "flex-column",
+          justifyContent: "center",
+          padding: "10%",
+        }}
+      >
+        <h1>{state}</h1>
+        <button onClick={incre}> page up</button>
+        <button onClick={decre}> page down </button>
+        {data.map((e, ind) => {
+          return (
+            <div key={ind}>
+              <h4>
+                {e.first_name} {e.last_name} - {e.email}
+              </h4>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
